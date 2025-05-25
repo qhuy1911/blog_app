a5f8bac79373979b8862cdec29fb76d746a311ef
Huy Nguyen <qhuy1911@gmail.com>
2025-05-25 11:46:23 +0700

    init and auth

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://eqjkzlmpddrlsjxqooln.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxamt6bG1wZGRybHNqeHFvb2xuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwNzQ2MDgsImV4cCI6MjA2MzY1MDYwOH0.Ymj8RHuLWW0irjJC23aKjIc-_cEWfkqQBrtm33e7V50';
+}
