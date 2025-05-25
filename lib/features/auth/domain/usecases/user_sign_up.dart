078f5f21c201762cbfed5a55461a38d38b39ff41
Huy Nguyen <qhuy1911@gmail.com>
2025-05-25 12:38:46 +0700

    signup usecase

---
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -0,0 +1,30 @@
+import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/src/either.dart';
+
+class UserSignUp implements Usecase<String, UserSignUpParams> {
+  final AuthRepository authRepository;
+  const UserSignUp(this.authRepository);
+
+  @override
+  Future<Either<Failures, String>> call(UserSignUpParams params) async {
+    return await authRepository.signUpWithEmailPassword(
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class UserSignUpParams {
+  final String name;
+  final String email;
+  final String password;
+
+  UserSignUpParams({
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+}
